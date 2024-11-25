ce0a103e99538c1f61f4ee4f7b3bb63b038ab2b6
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-25 21:24:28 +0545

    Update android15.sh

--- android15.sh
+++ android15.sh
@@ -28,6 +28,7 @@ rm -rf vendor/xiaomi/dynamicSpot
 rm -rf packages/apps/Updater
 rm -rf vendor/lineage
 rm -rf vendor/lineage-priv/keys
+rm -rf frameworks/native
 
 # Clone device-specific repositories
 git clone https://github.com/dpenra/device_xiaomi_sunny.git --depth 1 -b lineage-22 device/xiaomi/sunny
@@ -61,6 +62,7 @@ git clone https://gitlab.com/dpenra/android_vendor_xiaomi_mojito-leicacamera.git
 git clone https://github.com/extra-application/vendor_xiaomi_miuiapps.git --depth 1 -b main vendor/xiaomi/miuiapps
 git clone https://github.com/extra-application/vendor_xiaomi_dynamicSpot.git --depth 1 -b main vendor/xiaomi/dynamicSpot
 git clone https://github.com/dpenra/android_packages_apps_Updater.git --depth 1 -b fifteen packages/apps/Updater
+git clone https://github.com/dpenra/android_frameworks_native.git --depth 1 -b fifteen frameworks/native
 
 # Source modifications and RisingOS-specific keys
 git clone https://github.com/dpenra/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage

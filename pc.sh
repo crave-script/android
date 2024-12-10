a338b64e8a4853d3bd5012857e63d46a2cfecfe4
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-12-10 19:20:27 +0545

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -28,8 +28,8 @@ rm -rf vendor/bcr
 # rm -rf vendor/xiaomi/mojito-leicacamera
 # rm -rf vendor/xiaomi/miuiapps
 # rm -rf vendor/xiaomi/dynamicSpot
-# rm -rf packages/apps/Updater
-# rm -rf vendor/lineage
+rm -rf packages/apps/Updater
+rm -rf vendor/lineage
 # rm -rf vendor/lineage-priv/keys
 # rm -rf frameworks/native
 
@@ -71,13 +71,13 @@ git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor
 # git clone https://github.com/rising-source-mod/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage
 # git clone https://github.com/private-keys/vendor_lineage-priv_keys.git --depth 1 -b main vendor/lineage-priv/keys
 
-# cd vendor/lineage
-# curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/114e7aa0798e4f7bc0f8866c9979f9d5a2c72a66.patch | git am
-# curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/f8a153ad0138477c9cc2c57af95845d7d26ff1c3.patch | git am
-# cd ../..
-# cd packages/apps/Updater
-# curl -s https://github.com/RisingTechOSS/android_packages_apps_Updater/commit/9812469a2340abac46cbd839d5057d58125d9664.patch | git am
-# cd ../../..
+cd vendor/lineage
+curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/114e7aa0798e4f7bc0f8866c9979f9d5a2c72a66.patch | git am
+curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/f8a153ad0138477c9cc2c57af95845d7d26ff1c3.patch | git am
+cd ../..
+cd packages/apps/Updater
+curl -s https://github.com/RisingTechOSS/android_packages_apps_Updater/commit/9812469a2340abac46cbd839d5057d58125d9664.patch | git am
+cd ../../..
 # cd vendor/xiaomi/sunny
 # curl -s https://github.com/PixelOS-Devices/vendor_xiaomi_sunny/commit/592925300f9cd3946f39035ea438745255a5aa74.patch | git am
 # cd ../../..

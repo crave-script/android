41f5fb96515b4b08c9f63145b167cb8fa43ab604
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-12-10 08:18:39 +0545

    Update risingOS.sh

--- risingOS.sh
+++ risingOS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# curl https://raw.githubusercontent.com/crave-script/android15/refs/heads/main/android15.sh | bash
-curl https://raw.githubusercontent.com/crave-script/android15/refs/heads/main/zeyy.sh | bash
+curl https://raw.githubusercontent.com/crave-script/android15/refs/heads/main/android15.sh | bash
+# curl https://raw.githubusercontent.com/crave-script/android15/refs/heads/main/zeyy.sh | bash
